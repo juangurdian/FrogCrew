6241d3be9e2e67eb30ddd5370ed836052810d0c7
Alondra Oropeza <alondreza@gmail.com>
2025-03-24 21:28:12 -0500

    Add Spring Boot backend project

---
+++ frogcrew-backend/frogcrew-backend/src/main/java/com/frogcrew/backend/FrogcrewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.frogcrew.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogcrewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrogcrewBackendApplication.class, args);
+	}
+
+}
